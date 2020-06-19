49be2265def26004dcb6c0f641de021dc8aaa8d3
sainisajal147 <62516625+sainisajal147@users.noreply.github.com>
2020-06-20 01:31:52 +0530

    updated with js

---
+++ site/script.js
@@ -0,0 +1,6 @@
+console.log("FUCK I DONT KNOW EHAT IS GOinG ON");
+var he=function (){
+	var m=document.getElementById("n").value;
+	document.getElementById("fuck").textContent=m;
+	document.querySelector("#title").textContent=" awesome";
+};
