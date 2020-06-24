43043e57e6c13f0f91e20f94a8806f34fcc0d395
sainisajal147 <62516625+sainisajal147@users.noreply.github.com>
2020-06-24 19:22:18 +0530

    updated with jquery

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ console.log("FUCK I DONT KNOW EHAT IS GOinG ON");
 var he=function (){
 	var m=document.getElementById("n").value;
 	document.getElementById("fuck").textContent=m;
-	document.querySelector("#title").textContent=" awesome";
+	document.querySelector("#title").textContent="The Medicine Delivery system of "+m;
 };
